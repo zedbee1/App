f768edc31bcf21c3aafb31f29bd8d50336f67f6c
zedbee <c3mzahid@gmail.com>
2020-05-25 16:42:08 -0400

    2commit

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+     // 2nd commit
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- demo/src/main/java/com/example/demo/ServletInitializer.java
+++ demo/src/main/java/com/example/demo/ServletInitializer.java
@@ -5,6 +5,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
+	// 2nd commit
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(DemoApplication.class);
